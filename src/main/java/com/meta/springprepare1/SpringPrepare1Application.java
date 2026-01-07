95c3146a3ca16d0d83dcd97e76d82d8bc0d8afe3
Lim <apple2567338@gmail.com>
2026-01-07 14:39:20 +0900

    chore: First commit

---
+++ src/main/java/com/meta/springprepare1/SpringPrepare1Application.java
@@ -0,0 +1,13 @@
+package com.meta.springprepare1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrepare1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrepare1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/meta/springprepare1/SpringPrepare1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.springprepare1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepare1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
